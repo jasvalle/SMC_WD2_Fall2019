9c8325d3683a9c0aa5a75b0b04d0629878698d2b
Jasmine <valle_jasmine_lisett@student.smc.edu>
2019-12-10 15:33:31 -0800

    Final

--- Final/script.js
+++ Final/script.js
@@ -1,32 +1,35 @@
-    /*mobile nav*/
-    function openNav() {
-        document.getElementById("mySidenav").style.width = "100%";
+//mobile nav
+function openNav() {
+    document.getElementById("mySidenav").style.width = "100%";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
+
+var mySidenav = document.getElementById("mySidenav");
+var mySidenav = document.getElementById("sideNavToggle");
+
+sideNavToggle.addEventListener("click", function () {
+    if (mySidenav.clientWidth == window.clientWidth) {
+        closeNav();
+    } else {
+        openNav();
     }
+});
 
-    function closeNav() {
-        document.getElementById("mySidenav").style.width = "0";
-    }
+//donate button
+var btn = document.getElementById("donateBtn");
+var thankBox = document.getElementById("Message")
 
-    var mySidenav = document.getElementById("mySidenav");
-    var mySidenav = document.getElementById("sideNavToggle");
+donateBtn.addEventListener("click", function () {
+    thankBox.style.display = "block";
+});
 
-    sideNavToggle.addEventListener("click", function () {
-        if (mySidenav.clientWidth == window.clientWidth) {
-            closeNav();
-        } else {
-            openNav();
-        }
-    });
-
-    var btn = document.getElementById("donateBtn");
-    var thankBox = document.getElementById("thankBox")
-
-    donateBtn.addEventListener("click", function () {
-        thankBox.style.display = "block";
-    });
-
-    /*gallery text*/
-    $(".honey")
+$(document).ready(function () {
+    // CLICK, CAPTION DROPS
+    $("#honey")
         .click(function () {
-            $(this).children(".text").slideToggle("slow");
+            $("#text").slideToggle("slow");
         });
+});
---
+++ jquery.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    // CLICK, CAPTION DROPS
+    $(".honey")
+        .click(function () {
+            $(".text").slideToggle("slow");
+        });
+
+  
+});
