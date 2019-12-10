f25a7999c24d3a4f9b687907566d0da57f3bf77d
Jasmine <valle_jasmine_lisett@student.smc.edu>
2019-12-10 15:49:29 -0800

    Final

--- Final/script.js
+++ Final/script.js
@@ -1,4 +1,3 @@
-//mobile nav
 function openNav() {
     document.getElementById("mySidenav").style.width = "100%";
 }
@@ -16,20 +15,4 @@ sideNavToggle.addEventListener("click", function () {
     } else {
         openNav();
     }
-});
-
-//donate button
-var btn = document.getElementById("donateBtn");
-var thankBox = document.getElementById("Message")
-
-donateBtn.addEventListener("click", function () {
-    thankBox.style.display = "block";
-});
-
-$(document).ready(function () {
-    // CLICK, CAPTION DROPS
-    $("#honey")
-        .click(function () {
-            $("#text").slideToggle("slow");
-        });
 });
