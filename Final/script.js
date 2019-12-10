b0b5c7d90abaf5cace57c5c0ca6121a167131fe4
Jasmine <valle_jasmine_lisett@student.smc.edu>
2019-12-10 12:22:42 -0800

    Resources page added

--- Final/script.js
+++ Final/script.js
@@ -1,28 +1,32 @@
-function openNav() {
-    document.getElementById("mySidenav").style.width = "100%";
-}
-
-function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-}
-
-var mySidenav = document.getElementById("mySidenav");
-var mySidenav = document.getElementById("sideNavToggle");
-
-sideNavToggle.addEventListener("click", function(){
-    if(mySidenav.clientWidth == window.clientWidth){
-        closeNav();
+    /*mobile nav*/
+    function openNav() {
+        document.getElementById("mySidenav").style.width = "100%";
     }
 
-    else {
-    openNav();        
+    function closeNav() {
+        document.getElementById("mySidenav").style.width = "0";
     }
-});
 
-var btn = document.getElementById("donateBtn");
-var thankBox = document.getElementById("thankBox")
+    var mySidenav = document.getElementById("mySidenav");
+    var mySidenav = document.getElementById("sideNavToggle");
 
-donateBtn.addEventListener("click", function(){
-    thankBox.style.display = "block";
-});
+    sideNavToggle.addEventListener("click", function () {
+        if (mySidenav.clientWidth == window.clientWidth) {
+            closeNav();
+        } else {
+            openNav();
+        }
+    });
 
+    var btn = document.getElementById("donateBtn");
+    var thankBox = document.getElementById("thankBox")
+
+    donateBtn.addEventListener("click", function () {
+        thankBox.style.display = "block";
+    });
+
+    /*gallery text*/
+    $(".honey")
+        .click(function () {
+            $(this).children(".text").slideToggle("slow");
+        });
