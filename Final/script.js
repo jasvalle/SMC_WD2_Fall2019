4df916dfe85a550893bbf5c2e8e7fdee3c21ec15
Jasmine <valle_jasmine_lisett@student.smc.edu>
2019-12-09 20:31:24 -0800

    Final
    changed header picture to fit all screens, added large screen navs

--- Final/script.js
+++ Final/script.js
@@ -5,3 +5,24 @@ function openNav() {
 function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
 }
+
+var mySidenav = document.getElementById("mySidenav");
+var mySidenav = document.getElementById("sideNavToggle");
+
+sideNavToggle.addEventListener("click", function(){
+    if(mySidenav.clientWidth == window.clientWidth){
+        closeNav();
+    }
+
+    else {
+    openNav();        
+    }
+});
+
+var btn = document.getElementById("donateBtn");
+var thankBox = document.getElementById("thankBox")
+
+donateBtn.addEventListener("click", function(){
+    thankBox.style.display = "block";
+});
+
